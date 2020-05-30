3491d89b8ab40c5421ecb34f2c9210d826a6e2f5
Andy Moran <49261838+carlit-os@users.noreply.github.com>
2020-05-30 00:05:02 -0500

    Multi class classification works now.
    All provided test cases pass in well under a minute per data set. Achieved in ~2800 loops, anything lower would trip on Dataset 1

--- src/student_code.py
+++ src/student_code.py
@@ -29,8 +29,9 @@ def part_one_classifier(data_train, data_test):
     # The class value could be a 0 or a 1
     weight = [0, 0, 0]
     incorrect = 1
-
+    counter = 0
     while incorrect > 0:
+        counter += 1
         incorrect = 0
         for data in data_train:
             feature = [data[0], data[1], 1]
@@ -44,8 +45,19 @@ def part_one_classifier(data_train, data_test):
 
     for data in data_test:
         data[2] = dot_prod(weight, [data[0], data[1], 1])
-
     return
+
+
+def arg_max(weights, feature):
+    result = []
+    for weight in weights:
+        result.append(sum(x_i * y_i for x_i, y_i in zip(weight, feature)))
+
+    return result.index(max(result))
+
+
+def scale(feature, rate):
+    return [rate * i for i in feature]
 
 
 def part_two_classifier(data_train, data_test):
@@ -58,4 +70,25 @@ def part_two_classifier(data_train, data_test):
     # index 0 and Y in index 1, and a blank space in index 2
     # to be filled with class
     # The class value could be a 0 or a 8
+    rate = .01
+    weights = [[0, 0, 0] for x in range(9)]
+    loops = 0
+
+    while loops < 2800:
+        loops += 1
+
+        for data in data_train:
+            feature = [data[0], data[1], 1]
+            prediction = arg_max(weights, feature)  # arg_max should range 1-8
+            if prediction != data[2]:
+                weights[int(data[2])] = vec_add(weights[int(data[2])], scale(feature, rate))
+                weights[prediction] = vec_sub(weights[prediction], scale(feature, rate))
+
+    for data in data_test:
+        result = []
+        for weight in weights:
+            result.append(sum(x_i * y_i for x_i, y_i in zip(weight, [data[0], data[1], 1])))
+
+        data[2] = result.index(max(result))
+
     return
